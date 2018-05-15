ab9f4dadc6a98322a669055d34a27b6d9812e74a
Artur Tomczak <221666@edu.p.lodz.pl>
2018-05-15 13:20:58 +0200

    edycja wyglądu kodu oraz usunięcie otwierania ścieżki google podczas sprawdzania dostępności przeglądarki

--- src/test/java/ChatbotExistenceTest.java
+++ src/test/java/ChatbotExistenceTest.java
@@ -1,66 +1,32 @@
 import org.junit.Assert;
-/*import org.junit.Test;
-import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;*/
-import org.openqa.selenium.By;
-
-import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import org.openqa.selenium.ie.InternetExplorerDriver;
-
-import org.testng.annotations.AfterClass;
-
-import org.testng.annotations.AfterMethod;
-
-import org.testng.annotations.BeforeClass;
-
 import org.testng.annotations.BeforeMethod;
-
 import org.testng.annotations.Parameters;
-
 import org.testng.annotations.Test;
 
 public class ChatbotExistenceTest {
     WebDriver driver;
 
-    @Parameters("browser")// this annotation is used to insert browser parameter from TestNG xml
+    @Parameters("browser")
 
     @BeforeMethod
-    public void beforeTest(String browser)
-    {
+    public void beforeTest(String browser) {
 
-        if (browser.equalsIgnoreCase("firefox"))
-        {
+        if (browser.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
-        } else if (browser.equalsIgnoreCase("chrome"))
-        {
+        } else if (browser.equalsIgnoreCase("chrome")) {
             System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver");
             driver = new ChromeDriver();
-        }
-        else
-        {
+        } else {
             throw new IllegalArgumentException("The Browser Type is Undefined");
         }
-        driver.get("https://accounts.google.com/ServiceLogin?sacu=1 &scc=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&ss=1&ltmpl=default&rm=false");
     }
 
-
     @Test
-    public void titleChatPOL(){
-
-
-//        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver_win32\\chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
+    public void titleChatPOL() {
 
         driver.navigate().to("http://localhost:3006/");
 
@@ -70,30 +36,26 @@ public class ChatbotExistenceTest {
         driver.close();
         driver.quit();
     }
+
     @Test
-    public void chatExistence()    {
-/*        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();*/
+    public void chatExistence() {
 
         driver.navigate().to("http://localhost:3006/");
 
         Assert.assertTrue("checking if chat window exists",
-                driver.findElements( By.className("content")).size() != 0);
+                driver.findElements(By.className("content")).size() != 0);
 
         driver.close();
         driver.quit();
     }
 
     @Test
-    public void footerExistence(){
-/*        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();*/
+    public void footerExistence() {
 
         driver.navigate().to("http://localhost:3006/");
 
         Assert.assertTrue("checking if chat footer exists",
-                driver.findElements( By.className("footer")).size() != 0);
-
+                driver.findElements(By.className("footer")).size() != 0);
 
         driver.close();
         driver.quit();
